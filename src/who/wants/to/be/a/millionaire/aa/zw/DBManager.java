3364c0d0124cb5d289bdc5439a52696c26347800
AdhishAlva11 <qqv5855@aut.ac.nz>
2025-05-07 10:42:22 +1200

    Add DB Manager Class

--- src/who/wants/to/be/a/millionaire/aa/zw/DBManager.java
+++ src/who/wants/to/be/a/millionaire/aa/zw/DBManager.java
@@ -4,10 +4,35 @@
  */
 package who.wants.to.be.a.millionaire.aa.zw;
 
+
+import java.sql.*; 
 /**
  *
  * @author Adhis
  */
+
+
 public class DBManager {
-    
+    private static final String DB_URL = "jdbc:derby:database/HighScoresDB;create=true";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
+
+    public static void createTableIfNotExists() {
+        String sql = "CREATE TABLE HIGHSCORES (" +
+                     "NAME VARCHAR(50) PRIMARY KEY, " +
+                     "SCORE INT)";
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement()) {
+
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            if (!e.getSQLState().equals("X0Y32")) { // Table already exists
+                System.out.println("Table already exists.");
+            } else {
+                e.printStackTrace();
+            }
+        }
+    }
 }
