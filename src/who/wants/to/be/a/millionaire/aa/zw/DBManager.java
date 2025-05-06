00c350046c0135142a3a4c346f938209dd938097
AdhishAlva11 <qqv5855@aut.ac.nz>
2025-05-07 10:10:09 +1200

    Create DBManager.java

---
+++ src/who/wants/to/be/a/millionaire/aa/zw/DBManager.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package who.wants.to.be.a.millionaire.aa.zw;
+
+/**
+ *
+ * @author Adhis
+ */
+public class DBManager {
+    
+}
