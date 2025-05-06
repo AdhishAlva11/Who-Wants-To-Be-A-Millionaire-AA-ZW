c7a079441d4fd502e9d04607c3fde10c1256f6a3
Rupps101 <ziraakw@gmail.com>
2025-05-07 10:17:29 +1200

    Testing commit function

--- src/who/wants/to/be/a/millionaire/aa/zw/Main.java
+++ src/who/wants/to/be/a/millionaire/aa/zw/Main.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter player name: ");
+        System.out.print("Enter your name: ");
         String name = scanner.nextLine();
 
         String easyFile = "easy.txt";
