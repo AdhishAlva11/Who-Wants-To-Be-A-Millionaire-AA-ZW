9af0988f62261d14149268fc6be1ca0f839e673e
AdhishAlva11 <qqv5855@aut.ac.nz>
2025-05-12 20:22:32 +1200

    Starting conversion from Console to GUI
    Starting process of changing our code from console to GUI. Starting from main class

--- src/who/wants/to/be/a/millionaire/aa/zw/Main.java
+++ src/who/wants/to/be/a/millionaire/aa/zw/Main.java
@@ -4,13 +4,31 @@
  */
 package who.wants.to.be.a.millionaire.aa.zw;
 
+import java.awt.BorderLayout;
 import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
- *
+ *This class is the starting point of the program 
+ * it shows a simple GUI window where the user can input their name
+ * and click start game button to start game
  * @author Adhis
  */
-public class Main {
+public class Main  extends JFrame {
+    
+    // constructor sets up the GUI window 
+    public Main () 
+    {
+        setTitle("Who Wants to Be a Millionaire"); //window title
+        setSize(400, 250); // set window size (width x height) 
+        setLocationRelativeTo(null); // centre window on screen 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        setLayout(new BorderLayout());  // use BorderLayout for arranging compoenets
+        
+        // in the north we have the heading label 
+        JLabel titleLabel = new JLabel("Enter Your Name to Start: ", SwingConstants.CENTER); 
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your username: ");
